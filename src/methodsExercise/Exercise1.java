5c930d49f92931228b049ea8b300f983812e901e
Ion <vaniniabivol@gmail.com>
2021-02-05 20:46:53 +0100

    add some changes

--- src/methodsExercise/Exercise1.java
+++ src/methodsExercise/Exercise1.java
@@ -4,11 +4,11 @@ public class Exercise1 {
     public static void main(String[] args) {
 
         String firstName = "John";
-        String lastName = "Doe";
-        System.out.println(firstName.concat(lastName));  // Concatenare  -> alipirea unei valori
+        String lastName = " Doe";
+        System.out.println(firstName.concat(lastName));  // Concatenate  -> put together two strings
 
         String txt = "Hello Everybody";
-        System.out.println(txt.indexOf("d"));  // Aflarea pozitiei a unei valori dintr-un cuvaint
+        System.out.println(txt.indexOf("d"));  // Finding the position of a value in a word
 
 
         int x;
@@ -16,17 +16,20 @@ public class Exercise1 {
         int y = 10;
         int resultMax = Math.max(x, y);    // The highest value
         int resultMin = Math.min(x,y);
-        System.out.println(resultMin);    // The lowest value -> valoarea cea mai mica
+        System.out.println(resultMin);    // The lowest value
         System.out.println(resultMax);
 
         int a = 16;
-        double div = Math.sqrt(a);                   //Extragerea patratului
+        double div = Math.sqrt(a);                   //square extraction
         System.out.println(div);
 
         String[] cars = {"Volvo","BMw","Ford"};
-        System.out.println(cars.length);            //Lungimea Array
+        System.out.println(cars.length);
         for(String i : cars) {
             System.out.println(i);
+
+
+
 
 
         }
