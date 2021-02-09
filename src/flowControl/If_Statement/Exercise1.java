98fce8278b7668b6c25f3deeecfb37505f961249
Ion <vaniniabivol@gmail.com>
2021-02-09 13:11:22 +0100

    add if statement

---
+++ src/flowControl/If_Statement/Exercise1.java
@@ -0,0 +1,16 @@
+package flowControl.If_Statement;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        // check if a number is odd or even
+
+        int value = 7;
+        if (value % 2 == 0) {
+            System.out.println("The value: " + value + " is even ");
+        } else {
+            System.out.println("The value: " + "'" + value + "'" + " is odd ");
+        }
+    }
+}
---
+++ src/flowControl/If_Statement/TernaryOperator.java
@@ -0,0 +1,37 @@
+package flowControl.If_Statement;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        int hour = 13;
+
+        System.out.println("Before if statement");
+
+        if (hour > 17) {
+            System.out.println("It's evening");
+        } else {
+            System.out.println("it's good");
+        }
+
+        System.out.println("After if statement");
+
+
+        int number1 = 34;
+        int number2 = 45;
+        //ternary operator
+        int greaterNumber = number1 > number2 ? number2 : number1;
+
+        //converting using if statement
+        int greaterNum;
+        if (number1 > number2) {
+            greaterNum = number1;
+        } else {
+            greaterNum = number2;
+        }
+
+        System.out.println(greaterNum);
+
+
+
+    }
+}
